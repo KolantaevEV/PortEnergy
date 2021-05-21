6a95b41e45a7dff7c9aad7b358528c5cded4b9c3
RomanRV <sokolov.inf@gmail.com>
2021-05-21 18:28:26 +0300

    Add USART init / str2Char function

--- inc/typedefs.h
+++ inc/typedefs.h
@@ -1,6 +1,12 @@
 #ifndef INC_TYPEDEFS
 #define INC_TYPEDEFS
 
+#define DATA_BUF_SIZE 256
 
+typedef struct 
+{
+    char chars[36];
+    int size;
+} strData;
 
 #endif  /*INC_TYPEDEFS*/
--- src/interrupts.c
+++ src/interrupts.c
@@ -4,4 +4,7 @@ void IRQ_init(void)
 {
     //NVIC_SetPriority();
     //NVIC_EnableIRQ();
+    //NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+    //NVIC_SetPriority(USART2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 1, 0));
+    NVIC_EnableIRQ(USART2_IRQn);
 }
