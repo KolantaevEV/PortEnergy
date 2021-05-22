f050f9f84e0c79526210b530b923bd6b69e3c4da
SokolovRV <sokolov.inf@gmail.com>
2021-05-22 19:35:22 +0300

    Fix uart gpio.

--- lib/USART_LL/usart_ll.c
+++ lib/USART_LL/usart_ll.c
@@ -56,7 +56,7 @@ void uart2Init(void)
     GPIO_InitStruct.Pin = LL_GPIO_PIN_9 | LL_GPIO_PIN_10;
     GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
     GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
     GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
     LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
