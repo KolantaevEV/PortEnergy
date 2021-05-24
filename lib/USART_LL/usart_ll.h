2468e54e85e0cd47be14b45f29cf8789103414bc
KolantaevEV <kolantaevev@gmail.com>
2021-05-24 12:15:36 +0300

    r

--- lib/USART_LL/usart_ll.h
+++ lib/USART_LL/usart_ll.h
@@ -1,15 +1,3 @@
 #ifndef LIB_USART_LL
 #define LIB_USART_LL
 
-#include "stm32f1xx_ll_usart.h"
-#include "stm32f1xx_ll_rcc.h"
-#include "stm32f1xx_ll_bus.h"
-#include "stm32f1xx_ll_cortex.h"
-#include "stm32f1xx_ll_gpio.h"
-#include "typedefs.h"
-
-void uart2Init(void);
-buff str2Char(float temp, float volt);
-void USART2_IRQHandler(void);
-
-#endif  /*LIB_USART_LL*/
